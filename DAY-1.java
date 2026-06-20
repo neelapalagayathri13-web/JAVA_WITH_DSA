4fd1e4fd35816736b687614c9e23a27262743a45
Neelapala Gayathri <neelapalagayathri13@gmail.com>
2026-06-20 10:11:41 +0530

    Comment out example code in DAY-1.java
    Commented out code for even/odd and decimal to binary conversion.

---
+++ DAY-1.java
@@ -0,0 +1,40 @@
+//import java.util.*;
+class day1{
+    public static void main(String args[]){
+/* 
+
+        Scanner obj = new Scanner(System.in);
+        
+      EVEN OR ODD 
+
+        int a=obj.nextInt();
+        int b=obj.nextInt();
+        for(int i=1;i<=b;i++){
+            if((a&1)==0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+            }
+
+        }
+
+    */
+
+    /*  DECIMAL TO BINARY 
+       
+        int n=obj.nextInt();
+       String Binary =" ";
+       while(n>0) {
+            int rem=n%2;
+            Binary = Binary+rem;
+            n=n/2;
+       }
+       System.out.println(Binary);
+                                                           
+    */
+
+       
+        
+    }
+}
